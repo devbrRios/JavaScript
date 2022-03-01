0c979167565db3d124e429bee398cc2c168ce530
Bruno Santana Rios <91757916+devbrRios@users.noreply.github.com>
2022-03-01 19:22:14 -0300

    aula 15

---
+++ aula15/ambiente.js
@@ -0,0 +1,26 @@
+let num = [];
+/*num[0]=6;
+num[1]=19;
+num.push(60);*/
+
+for (let c = 15; c >= 1; c -= 3) {
+    num.push(c)
+}
+
+console.log(`Nosso vetor é o ${num}`)
+console.log(`O tamanho do array é ${num.length}`)
+num.sort();
+console.log(`O array ordenado é ${num}`)
+
+//For mais simplificado para usar em array e object
+
+for (let pos in num) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+
+/*quando o valor existe dentro do array existir dentro do array
+o metodo indexOf vai retornar sua posição,
+caso nao exista irá retornar '-1'
+*/
+let cons = num.indexOf(10)
+console.log(`O valor 10 está na posição ${cons}`)
